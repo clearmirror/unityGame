ebe5f9ef5609850fcfea62aa57381770b75acdb6
ychen <chenyongzhick@gmail.com>
2017-10-22 17:43:00 -0400

    Combat menu

---
+++ Assets/Scripts/BattleInputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleInputManager : MonoBehaviour {
+
+    // public Button yourButton;
+
+    // Use this for initialization
+    private GameObject player;
+    private GameObject enemy;
+    
+    void Start () {
+        player = GameObject.FindWithTag("Player");
+        enemy = GameObject.FindWithTag("Enemy");
+    }
+
+    public void TaskOnClick()
+    {
+        PlayerControl script = player.GetComponent<PlayerControl>();
+        script.MoveTo(enemy.transform.position);
+    }
+}
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,19 +4,69 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
 
-    private Rigidbody rb;
+    private GameObject rb;
+    private GameObject commandMenu;
 
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
-	}
-	
+    
+    private bool isMoving;
+    private Vector3 moveDiff;
+    private Vector3 targetPos;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GameObject.FindWithTag("Player");
+        commandMenu = GameObject.FindWithTag("commandMenu");
+        isMoving = false;
+    }
+
+    public void MoveTo(Vector3 pos)
+    {
+        isMoving = true;
+        targetPos = pos;
+        Vector3 currentPos = rb.transform.position;
+        float speed = 10.0f;
+        float timeNeeded = Vector3.Distance(pos, currentPos) / speed;
+        float secPerFrame = 1.0f / 60;
+
+        Vector3 diffVector = pos - currentPos;
+        Vector3 vectorPerFrame = diffVector / timeNeeded * secPerFrame;
+        moveDiff = vectorPerFrame;
+        Debug.Log(moveDiff);
+        Debug.Log(diffVector);
+        Debug.Log(timeNeeded);
+        Debug.Log(diffVector / timeNeeded);
+    }
+
+    bool AlmostToDestination()
+    {
+        Vector3 currentPos = rb.transform.position;
+        return Vector3.Distance(currentPos, targetPos) < 1.0f;
+    }
 	// Update is called once per frame
-	void FixedUpdate () {
+	void Update () {
+        
+        if(isMoving)
+        {
+            rb.transform.Translate(moveDiff);
+            if (AlmostToDestination())
+            {
+                isMoving = false;
+            }
+            return;
+        }
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertial = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertial);
 
-        rb.AddForce(movement);
-	}
+        Vector3 distance = new Vector3(-6.0f, 0.0f, -4.0f);
+        commandMenu.transform.position = rb.transform.position + distance;
+
+        if (moveHorizontal == 0.0f && moveVertial == 0.0f)
+        {
+            return;
+        }
+        rb.transform.Translate(movement);
+    }
 }
